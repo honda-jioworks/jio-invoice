4a6f7df5a6ccea3f6e20cfd7697922459c44957c
honda-jioworks <honda@jioworks.jp>
2022-09-30 17:10:57 +0900

    ダッシュボードの上部をそれっぽく実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>ダッシュボードを置く想定</div>
+  <div><DashBoard /></div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
-@Component({})
-export default class IndexPage extends Vue {}
+import DashBoard from '~/components/templates/DashBoard.vue'
+
+@Component({
+  components: { DashBoard },
+})
+export default class IndexPage extends Vue {
+  // pages内でコンテンツ制御（データ取得・更新などのロジックのみ）を管轄する
+}
 </script>
