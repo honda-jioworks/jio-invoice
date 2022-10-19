57e7b2462b898c9bfce958fb7eb821f02cc2a606
honda-jioworks <honda@jioworks.jp>
2022-10-19 21:29:41 +0900

    ゴルフポータル（仮）をコミット

---
+++ pages/jiogolf.vue
@@ -0,0 +1,15 @@
+<template>
+  <GolfDashBoard />
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import GolfDashBoard from '@/components/templates/GolfDashBoard.vue'
+
+@Component({
+  components: { GolfDashBoard },
+})
+export default class JioGolf extends Vue {
+  // pages内でコンテンツ制御（データ取得・更新などのロジックのみ）を管轄する
+}
+</script>
