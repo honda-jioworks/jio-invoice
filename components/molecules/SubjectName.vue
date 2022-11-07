d43ebc887f4ad32cb329b3c8416f95b05faac395
Konno98 <y_konno@jioworks.jp>
2022-11-07 14:05:49 +0900

    第4回コミット

--- components/molecules/SubjectName.vue
+++ components/molecules/SubjectName.vue
@@ -2,15 +2,15 @@
   <div>
     <v-row>
       <v-col cols="6">項目名:</v-col>
-      <v-col cols="6"><TextBox @input-test="inputTest" /></v-col>
+      <v-col cols="6"><SubjectNameTextBox @input-test="inputTest" /></v-col>
     </v-row>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator';
-import TextBox from '@/components/atoms/input/TextBox.vue';
-@Component({ components: { TextBox } })
+import SubjectNameTextBox from '~/components/atoms/input/SubjectNameTextBox.vue';
+@Component({ components: { SubjectNameTextBox } })
 export default class SubjectName extends Vue {
   private textval: string = '';
   inputTest(val: string): void {
