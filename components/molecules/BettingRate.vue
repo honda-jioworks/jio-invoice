a7886a752c6ad5ab367a96d06e4b6c3023f97bef
TaiseiShinozaki <shinozakitaisei00@gmail.com>
2022-11-08 15:10:15 +0900

    バリデーション桁数制限実装

--- components/molecules/BettingRate.vue
+++ components/molecules/BettingRate.vue
@@ -15,7 +15,11 @@ export default class BettingRate extends Vue {
   inputRules: Array<object> = [
     (v: string) => !!v || '項目を入力してください',
     (v: string) => /^([1-9]\d*|0)$/.test(v) || '数値を入力してください',
-    (v: string) => (v && v.length <= 3) || '3桁以内で入力してください',
+    (v: string) => this.numberCheck(v),
   ];
+
+  private numberCheck(value: string): any {
+    return (value && value.length <= 3) || '3桁以内で入力してください';
+  }
 }
 </script>
