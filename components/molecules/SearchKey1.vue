2ddabda9ecf4a902f3bd75d9637f798e7eb6cc5c
yokomizo-jio <d_yokomizo@jioworks.jp>
2022-11-17 10:26:05 +0900

    従業員フォーム作成

---
+++ components/molecules/SearchKey1.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-form>
+      <v-container>
+        <v-row>
+            <v-col
+                cols="12"
+                md="4"
+                ><v-text-field 
+                :rules="nameRules"
+                label="検索キー1"
+                required
+                ></v-text-field>
+            </v-col>
+        </v-row>
+    </v-container>
+  </v-form>
+  </template>
+  
+  <script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+@Component({})
+export default class SearchKey1 extends Vue {
+      nameRules: Array<object> = [
+        (v:string) => !!v || '入力してください'
+      ];
+    }
+</script>
--- components/templates/DashBoard.vue
+++ components/templates/DashBoard.vue
@@ -16,8 +16,8 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator'
-import MostRecentSalesAmount from '@/components/organisms/MostRecentSalesAmount.vue'
+import { Vue, Component } from 'nuxt-property-decorator';
+import MostRecentSalesAmount from '@/components/organisms/MostRecentSalesAmount.vue';
 @Component({ components: { MostRecentSalesAmount } })
 export default class DashBoard extends Vue {
   // templatesはページの大まかな配置（ワイヤーフレーム）を管轄
