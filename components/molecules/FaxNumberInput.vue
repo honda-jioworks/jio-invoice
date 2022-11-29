e8eb2bf16949072a623d2a70cd8a62d50c5ba506
yokomizo-jio <d_yokomizo@jioworks.jp>
2022-11-29 17:28:29 +0900

    organismsへデータ移行

--- components/molecules/FaxNumberInput.vue
+++ components/molecules/FaxNumberInput.vue
@@ -2,14 +2,24 @@
   <v-form>
     <v-row>
       <v-col cols="12" md="4">
-        <v-text-field label="FAX："></v-text-field>
+        <v-text-field :rules="nameRules" label="FAX" @change="getFaxnumber"></v-text-field>
       </v-col>
     </v-row>
   </v-form>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator';
+import { Vue, Component, Emit } from 'nuxt-property-decorator';
 @Component({})
-export default class FaxNumberInput extends Vue {}
+export default class FaxNumberInput extends Vue {
+  nameRules: Array<object> = [
+    (v: string) => !!v || 'FAX番号を入力してください',
+    (v: string) => /^0\d{1,3}-\d{2,4}-\d{3,4}$/.test(v) || 'ハイフンと数値を入力してください',];
+  faxnumber: string = "";
+
+  @Emit()
+    getFaxnumber(faxnumber: string): string{
+      return faxnumber;
+    }
+}
 </script>
--- components/molecules/PostBox.vue
+++ components/molecules/PostBox.vue
@@ -2,15 +2,21 @@
   <v-form>
     <v-row>
       <v-col cols="12" md="6">
-        <v-text-field :rules="nameRules" label="役職："></v-text-field>
+        <v-text-field :rules="nameRules" label="役職" @change="getPostbox"></v-text-field>
       </v-col>
     </v-row>
   </v-form>
 </template>
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator';
+import { Vue, Component, Emit } from 'nuxt-property-decorator';
 @Component({})
 export default class PostBox extends Vue {
   nameRules: Array<object> = [(v: string) => !!v || '役職を入力してください'];
+  postbox: string = "";
+
+  @Emit()
+  getPostbox(postbox: string): string{
+    return postbox;
+  }
 }
 </script>
