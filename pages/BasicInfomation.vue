f3842a2373f955e4e4fa82233ef4d35a965b3fcc
Konno98 <y_konno@jioworks.jp>
2022-12-19 13:24:07 +0900

    エラー処理

--- pages/BasicInfomation.vue
+++ pages/BasicInfomation.vue
@@ -1,96 +1,85 @@
 <template>
-    <v-sheet class="groundwork">
-      <v-row>
-        <v-col cols="12">
-          <CustomerName @scan-name="scanName" />
-          <CustomerNameKana @scan-kana="scanKana" />
-          <AbbreviationInput @scan-short="scanShort" />
-          <PrimarySearchInput @scan-primary="scanPrimary" />
-          <SecondSearchInput @scan-second="scanSecond" />
-          <MemoBox @scan-memo="scanMemo" />
-          <NotShowBox @scan-check="scanCheck" />
-          <SaveCancelButton />
-        </v-col>
-      </v-row>
-    </v-sheet>
-  </template>
-  
-  <script lang="ts">
-  import { Vue, Component } from 'nuxt-property-decorator';
-  import CustomerName from '@/components/molecules/CustomerName.vue';
-  import CustomerNameKana from '@/components/molecules/CustomerNameKana.vue';
-  import AbbreviationInput from '~/components/molecules/AbbreviationInput.vue';
-  import PrimarySearchInput from '@/components/molecules/PrimarySearchInput.vue';
-  import SecondSearchInput from '@/components/molecules/SecondSearchInput.vue';
-  import MemoBox from '@/components/molecules/MemoBox.vue';
-  import NotShowBox from '~/components/molecules/NotShowBox.vue';
-  import SaveCancelButton from '~/components/molecules/SaveCancelButton.vue';
+  <v-sheet class="groundwork">
+    <v-row>
+      <v-col cols="12">
+        <CustomerName @scan-name="scanName" />
+        <CustomerNameKana @scan-kana="scanKana" />
+        <AbbreviationInput @scan-short="scanShort" />
+        <PrimarySearchInput @scan-primary="scanPrimary" />
+        <SecondSearchInput @scan-second="scanSecond" />
+        <MemoBox @scan-memo="scanMemo" />
+        <NotShowBox @scan-check="scanCheck" />
+      </v-col>
+    </v-row>
+  </v-sheet>
+</template>
 
-  @Component({
-    components: {
-      CustomerName,
-      CustomerNameKana,
-      AbbreviationInput,
-      PrimarySearchInput,
-      SecondSearchInput,
-      MemoBox,
-      NotShowBox,
-      SaveCancelButton
-    },
-  })
-  export default class BasicInfomation extends Vue {
-    private nameVal: string = '';
-    private kanaVal: string = '';
-    private shortVal: string = '';
-    private primaryVal: string = '';
-    private secondVal: string = '';
-    private memoVal: string = '';
-    private checkVal: boolean = false;
-    private saveVal: string = '';
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+import CustomerName from '@/components/molecules/CustomerName.vue';
+import CustomerNameKana from '@/components/molecules/CustomerNameKana.vue';
+import AbbreviationInput from '~/components/molecules/AbbreviationInput.vue';
+import PrimarySearchInput from '@/components/molecules/PrimarySearchInput.vue';
+import SecondSearchInput from '@/components/molecules/SecondSearchInput.vue';
+import MemoBox from '@/components/molecules/MemoBox.vue';
+import NotShowBox from '~/components/molecules/NotShowBox.vue';
 
-    saveButton(val: string): void {
-        this.saveVal = val;
-    }
+@Component({
+  components: {
+    CustomerName,
+    CustomerNameKana,
+    AbbreviationInput,
+    PrimarySearchInput,
+    SecondSearchInput,
+    MemoBox,
+    NotShowBox,
+  },
+})
+export default class BasicInfomation extends Vue {
+  private nameVal: string = '';
+  private kanaVal: string = '';
+  private shortVal: string = '';
+  private primaryVal: string = '';
+  private secondVal: string = '';
+  private memoVal: string = '';
+  private checkVal: boolean = false;
+  private saveVal: string = '';
 
-    scanName(val: string): void {
-      this.nameVal = val;
-      console.log(this.nameVal);
-    }
-  
-    scanKana(val: string): void {
-      this.kanaVal = val;
-      console.log(this.kanaVal);
-    }
-  
-    scanShort(val: string): void {
-      this.shortVal = val;
-      console.log(this.shortVal);
-    }
-  
-    scanPrimary(val: string): void {
-      this.primaryVal = val;
-      console.log(this.primaryVal);
-    }
-  
-    scanSecond(val: string): void {
-      this.secondVal = val;
-      console.log(this.secondVal);
-    }
-  
-    scanMemo(val: string): void {
-      this.memoVal = val;
-      console.log(this.memoVal);
-    }
-  
-    scanCheck(val: boolean): void {
-      this.checkVal = val;
-      console.log(this.checkVal);
-    }
+  saveButton(val: string): void {
+    this.saveVal = val;
   }
-  </script>
-  <style lang="scss" scoped>
-  .groundwork {
-    background-color: white;
+
+  scanName(val: string): void {
+    this.nameVal = val;
   }
-  </style>
-  
+
+  scanKana(val: string): void {
+    this.kanaVal = val;
+  }
+
+  scanShort(val: string): void {
+    this.shortVal = val;
+  }
+
+  scanPrimary(val: string): void {
+    this.primaryVal = val;
+  }
+
+  scanSecond(val: string): void {
+    this.secondVal = val;
+  }
+
+  scanMemo(val: string): void {
+    this.memoVal = val;
+  }
+
+  scanCheck(val: boolean): void {
+    this.checkVal = val;
+  }
+}
+</script>
+<style lang="scss" scoped>
+.groundwork {
+  background-color: white;
+}
+</style>
