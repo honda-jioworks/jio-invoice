4c214f032865b47d46da01d138ab018c8ff61514
yokomizo-jio <d_yokomizo@jioworks.jp>
2022-12-20 17:29:48 +0900

    新コンポーネント作成

---
+++ components/molecules/TaxProcessingForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-row>
+        <v-col>
+            <v-select :items="items" label="税処理" ></v-select>
+        </v-col>
+    </v-row>
+  </template>
+  
+  <script lang="ts">
+  import { Vue, Component,} from 'nuxt-property-decorator';
+  @Component({ components: {} })
+  export default class PicName extends Vue {
+    items: Array<string> = ['内税','外税','合計請求','税なし'];
+  }
+</script>
+
+  
