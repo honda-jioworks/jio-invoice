5e1033676b0e3874ab916e5c4710116357a32e03
yokomizo-jio <d_yokomizo@jioworks.jp>
2022-12-21 23:48:02 +0900

    請求書発行画面1部

--- components/molecules/CalendarForm.vue
+++ components/molecules/CalendarForm.vue
@@ -1,15 +1,20 @@
 <template>
     <v-row>
       <v-col cols="4">
-          <v-text-field type="date"/>
+          <v-text-field type="date" v-model="date" @input="scanDate"/>
       </v-col>
     </v-row>
 </template>
 
 <script lang="ts">
-  import { Vue, Component} from 'nuxt-property-decorator';
+  import { Vue, Component, Emit} from 'nuxt-property-decorator';
   @Component({  })
   export default class CalendarForm extends Vue {
-    
+    date: string ='';
+  
+  @Emit()
+  scanDate(): string{
+    return this.date
+  }
   }
 </script>
