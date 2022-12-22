d760be76202947c0ded6a1268d0adec92ec2670a
koizumianri <a_koizumi@jioworks.jp>
2022-12-22 16:46:09 +0900

    請求書一覧画面細かい修正

--- components/molecuels/Register.vue
+++ components/molecuels/Register.vue
@@ -1,3 +1,18 @@
 <template>
-  <v-btn>新規</v-btn>
+  <v-btn @click="open">新規</v-btn>
+ 
 </template>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+@Component({})
+export default class Register extends Vue {
+
+   open(): void {
+    let result = window.confirm("一覧ページへ移動します。よろしいですか?")
+    if( result ){
+      location.href ="/";
+    }
+  }
+}
+
+</script>
