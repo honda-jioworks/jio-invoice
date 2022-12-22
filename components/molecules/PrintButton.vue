3501da9d3e22842f6fb4d1cdd57435271a0963f1
yokomizo-jio <d_yokomizo@jioworks.jp>
2022-12-22 11:44:44 +0900

    仮コミット

---
+++ components/molecules/PrintButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-btn >印刷</v-btn>
+</template>
+
+  <script lang="ts">
+  import { Vue, Component} from 'nuxt-property-decorator';
+  @Component({ })
+  export default class AddressForm extends Vue {
+    
+ }
+</script>
