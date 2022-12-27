38de2262bca55bd6bcb649b56edcda65c73e4f75
t_onoue@jioworks.jp <t_onoue@jioworks.jp>
2022-12-27 18:39:43 +0900

    ファイル消去

--- components/molecules/CEOName.vue
+++ components/molecules/CEOName.vue
@@ -10,9 +10,8 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'nuxt-property-decorator';
-import CEONameLabel from '~/components/atoms/label/CEONameLabel.vue';
 import CEONameTextBox from '~/components/atoms/input/CEONameTextBox.vue';
-@Component({ components: { CEONameTextBox, CEONameLabel } })
+@Component({ components: { CEONameTextBox } })
 export default class CEOName extends Vue {
   private name: string = '';
 
